9b327650f15f1bdb65e3147e13467f843e873782
john ko <johnsko89@gmail.com>
2015-02-01 17:28:36 -0500

    got youtube integration for each champion

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -8,6 +8,7 @@ def show
   champ_id = params[:id].to_i
   clean_data = Champion.data
   @champ_info = clean_data.find {|f| f["id"] == champ_id}
+  @url = Champion.get_url(@champ_info["name"])
 end
 
 end
--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,3 +1,5 @@
+require 'google/api_client'
+
 class Champion < ActiveRecord::Base
 
   def self.data
@@ -16,5 +18,57 @@ class Champion < ActiveRecord::Base
     end
     return clean_data
   end
+
+
+  DEVELOPER_KEY = ENV["YOUTUBE_API_KEY"]
+  YOUTUBE_API_SERVICE_NAME = 'youtube'
+  YOUTUBE_API_VERSION = 'v3'
+
+  def self.get_service
+    client = Google::APIClient.new(
+    :key => DEVELOPER_KEY,
+    :authorization => nil,
+    :application_name => 'Johns APP',
+    :application_version => '0.0.1'
+    )
+    youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
+
+    return client, youtube
+  end
+
+  def self.get_url(clip)
+    opts = Trollop::options do
+      opt :q, '#{clip}', :type => String, :default => clip + 'champion spotlight'
+      opt :max_results, 'Max results', :type => :int, :default => 1
+    end
+
+    client, youtube = Champion.get_service
+
+    begin
+      # Call the search.list method to retrieve results matching the specified
+      # query term.
+      search_response = client.execute!(
+      :api_method => youtube.search.list,
+      :parameters => {
+        :part => 'snippet',
+        :q => opts[:q],
+        :maxResults => opts[:max_results]
+      }
+      )
+
+      videos = []
+      # Add each result to the appropriate list, and then display the lists of
+      # matching videos, channels, and playlists.
+      search_response.data.items.each do |search_result|
+        case search_result.id.kind
+        when 'youtube#video'
+          videos << "//youtube.com/embed/#{search_result.id.videoId}"
+        end
+      end
+      url = videos[0]
+      url
+    end
+  end
+
 
 end
